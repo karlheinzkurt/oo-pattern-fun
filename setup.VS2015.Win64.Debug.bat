de8c346444c4e35fe7349bbb1838a6444a24a13d
Sascha Lumma <s.lumma@gmail.com>
2017-01-14 11:25:35 +0100

    Fixed build scripts to avoid confusion on platform, compiler, configuration and ide

--- setup.VS2015.Win64.Debug.bat
+++ setup.VS2015.Win64.Debug.bat
@@ -1,13 +1,15 @@
 @echo off
 set DIR=%~dp0
-set BUILD_DIR="%DIR%\build\VS2015"
+set BUILD_DIR="%DIR%\build\VS2015\Win64\Debug"
 set SOURCE_DIR="%DIR%"
 
 md "%BUILD_DIR%"
 pushd "%BUILD_DIR%"
    cmake ^
+   -DCOMPILER_ID=VS2015^
+   -DBIN_PATH_POSTFIX=VS2015/Win64^
    -DCMAKE_BUILD_TYPE=Debug^
-   -G"Visual Studio 14 2015"^
+   -G"Visual Studio 14 2015 Win64"^
     "%SOURCE_DIR%"
 popd
 
