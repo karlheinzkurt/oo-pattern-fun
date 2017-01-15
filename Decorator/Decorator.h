662bcafcaeff14229e5447833428088bd702ffb8
Sascha Lumma <s.lumma@gmail.com>
2017-01-15 20:21:56 +0100

    Improved decorator

--- Decorator/Decorator.h
+++ Decorator/Decorator.h
@@ -3,27 +3,49 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <list>
+#include <algorithm>
 
 namespace API
 {
-   struct TextInterface
+   struct Text
    {
-      virtual ~TextInterface() = default;
+      virtual ~Text() = default;
       
       virtual void setText(std::string text) = 0;
 
       virtual std::string toString() const = 0;
    };
    
-   std::ostream& operator<<( std::ostream& os, TextInterface const& text )
+   std::ostream& operator<<( std::ostream& os, Text const& text )
    {  return os << text.toString(); }
 } // API
 
+namespace Detail
+{
+   typedef std::list<std::string> LineListType;
+   
+   LineListType SplitLines(std::string text)
+   {
+      std::istringstream s(text);
+      std::string item;
+      LineListType lines;
+      while (std::getline(s, item, '\n')) { lines.push_back(item); }
+      return std::move(lines);
+   }
+   
+   size_t GetMaximumLength(LineListType const& lines)
+   {
+      auto const element(std::max_element(lines.begin(), lines.end(), [](auto const& a, auto const& b){ return a.size() < b.size(); }));
+      return (element == lines.end() ? 0 : element->size());
+   }
+}
+
 namespace Concrete
 {
-   struct TextObject : public API::TextInterface
+   struct Text : public API::Text
    {
-      TextObject( std::string const& text ) : m_text( text ) {}
+      Text( std::string const& text ) : m_text( text ) {}
 
       virtual void setText(std::string text) override
       {  m_text = text; }
@@ -35,39 +57,47 @@ namespace Concrete
       std::string m_text;
    };
 
-   struct PipeDecorator : public API::TextInterface
+   struct PipeDecorator : public API::Text
    {
-      PipeDecorator( std::unique_ptr<API::TextInterface> decoratee ) : m_decoratee(std::move(decoratee)) {} 
+      PipeDecorator( std::unique_ptr<API::Text> decoratee ) : m_decoratee(std::move(decoratee)) {} 
 
       virtual void setText(std::string text) override
       {  m_decoratee->setText(text); }
 
       virtual std::string toString() const override
       {  
+         auto lines(Detail::SplitLines(m_decoratee->toString()));
+         auto const length(Detail::GetMaximumLength(lines));
+         
          std::ostringstream os;
-         os << "|"  << m_decoratee->toString() << "|";
+         for (auto const& l : lines) { os << '|' << l << std::string(length - l.size(), ' ') << "|\n"; }
          return os.str();
       }
       
    private:
-      std::unique_ptr<API::TextInterface> m_decoratee;
+      std::unique_ptr<API::Text> m_decoratee;
    };
    
-   struct MinusDecorator : public API::TextInterface
+   struct BoxDecorator : public API::Text
    {
-      MinusDecorator( std::unique_ptr<API::TextInterface> decoratee ) : m_decoratee(std::move(decoratee)) {} 
+      BoxDecorator(std::unique_ptr<API::Text> decoratee) : m_decoratee(std::move(decoratee)) {} 
 
       virtual void setText(std::string text) override
       {  m_decoratee->setText(text); }
 
       virtual std::string toString() const override
       {  
+         auto lines(Detail::SplitLines(m_decoratee->toString()));
+         auto const length(Detail::GetMaximumLength(lines));
+         lines.emplace_front(std::string(length, '-'));
+         lines.emplace_back(lines.front());
+         
          std::ostringstream os;
-         os << "-"  << m_decoratee->toString() << "-";
+         for (auto const& l : lines) { os << '|' << l << std::string(length - l.size(), ' ') << "|\n"; }
          return os.str();
       }
-      
-   private:
-      std::unique_ptr<API::TextInterface> m_decoratee;
+     
+   private:   
+      std::unique_ptr<API::Text> m_decoratee;
    };
 } // Concrete
--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -11,12 +11,19 @@ int main( int argc, char** argv )
     *  so we can re-assign the decorated object to the same pointer.
     */
 
-   std::unique_ptr<API::TextInterface> text(new TextObject("some text"));
+   std::unique_ptr<API::Text> text(new Text("some text"));
    std::cout << *text << '\n';
    
-   text = std::make_unique<PipeDecorator>(std::make_unique<MinusDecorator>(std::make_unique<PipeDecorator>(std::move(text))));
+   text = std::make_unique<BoxDecorator>(std::make_unique<PipeDecorator>(std::move(text)));
    std::cout << *text << '\n';
    
-   text->setText("blub");
+   text->setText("some other\ntext");
+   std::cout << *text << '\n';
+   
+   for (size_t c(0); c < 5; ++c) { text = std::make_unique<PipeDecorator>(std::move(text)); }
+   std::cout << *text << '\n';
+   
+   text = std::make_unique<BoxDecorator>(std::move(text));
+   text->setText("blub\nblub blub\nblub");
    std::cout << *text << '\n';
 }
